8c02fc450dcd63a59ded29fa3f93ff1404f7233b
David Matthews <dmatthe1@uvm.edu>
2021-02-26 18:23:25 -0500

    updates to plotting

--- henon_bifurcation.py
+++ henon_bifurcation.py
@@ -9,16 +9,17 @@ import tqdm
 
 ti.init(arch=ti.cpu)
 
-amin = 1.935
-amax = 1.952
+amin = 1.0
+amax = 2.0
+# amin = 1.935
+# amax = 1.952
 # amin = 1.964
 # amax = 1.967
 b = -0.3
-burnin = int(1e6)
+burnin = 1<<24
 
-NUM_A = 1<<13 # 1<<15
+NUM_A =  1<<15
 sample_len = NUM_A<<2
-
 da = (amax - amin)/NUM_A
 
 IC = (0.0, 2.0)
@@ -27,19 +28,19 @@ xmin = -0.75
 xmax = 2.25
 xrange = xmax - xmin
 
-pixels = ti.field(ti.f32, (NUM_A, NUM_A))
-x_field = ti.field(ti.f32, (NUM_A, NUM_A))
+pixels = ti.field(ti.u8, (NUM_A, NUM_A))
+# x_field = ti.field(ti.f32, (NUM_A, NUM_A * 1<<ASPECT_RATIO))
 
 @ti.kernel
 def henon_iterate(burnin: ti.i32,
                     sample_len: ti.i32,
-                    amin: ti.f32,
-                    amax: ti.f32,
-                    da: ti.f32,
+                    amin: ti.f64,
+                    amax: ti.f64,
+                    da: ti.f64,
                     n_threads: ti.i32):
     for tid in range(n_threads):
-        x = IC[0]
-        y = IC[1]
+        x = ti.cast(IC[0], ti.f64)
+        y = ti.cast(IC[1], ti.f64)
         a = amin + tid * da
         for i in range(burnin):
             x_tmp = a - x*x + b * y
@@ -49,31 +50,33 @@ def henon_iterate(burnin: ti.i32,
             x_tmp = a - x*x + b * y
             y = x
             x = x_tmp
-            x_field[tid, i] = x
+            # x_field[tid, i] = x
             px_row = int((x - xmin)/xrange * float(NUM_A))
-            pixels[tid, px_row] = 1.0
+            pixels[tid, px_row] = ti.cast(1, ti.u8)
 
 
+print("Running henon_map")
 henon_iterate(burnin, sample_len, amin, amax, da, NUM_A)
+print("Done. Now plotting")
 pixels_np = pixels.to_numpy()
 
-print("now plotting...")
-SUBSAMPLE = 2
+# print("now plotting...")
+# SUBSAMPLE = 2
 
-fig, ax = plt.subplots(figsize=(10,10))
-ax.imshow(pixels_np[::SUBSAMPLE, ::-1*SUBSAMPLE].T, cmap="gray", interpolation="nearest")
+# fig, ax = plt.subplots(figsize=(10,10))
+# ax.imshow(pixels_np[::SUBSAMPLE, ::-1*SUBSAMPLE].T, cmap="gray", interpolation="nearest")
 
-# locs, labels = plt.xticks()
-locs = range(0, NUM_A//SUBSAMPLE, (NUM_A//SUBSAMPLE)//40)
-plt.xticks(locs, ["{:.5f}".format(amin + da *loc*SUBSAMPLE) for loc in locs], rotation=90)
+# # locs, labels = plt.xticks()
+# locs = range(0, NUM_A//SUBSAMPLE, (NUM_A//SUBSAMPLE)//40)
+# plt.xticks(locs, ["{:.5f}".format(amin + da *loc*SUBSAMPLE) for loc in locs], rotation=90)
 
-locs, labels = plt.yticks()
-plt.yticks(locs, ["{:.6f}".format(((pixels_np.shape[1] - loc *  SUBSAMPLE)* xrange)/NUM_A + xmin) for loc in locs])
-plt.savefig("q5_labeled.png", dpi=NUM_A/5, bbox_inches="tight")
-# plt.show()
+# locs, labels = plt.yticks()
+# plt.yticks(locs, ["{:.6f}".format(((pixels_np.shape[1] - loc *  SUBSAMPLE)* xrange)/NUM_A + xmin) for loc in locs])
+# plt.savefig("q5_labeled.png", dpi=NUM_A/5, bbox_inches="tight")
+# # plt.show()
 
 
 
-# im = Image.fromarray(pixels_np[:, ::-1].T.astype('uint8')*255)
-# im.save("q5_raw.png")
-# # im.show()
+im = Image.fromarray(255 - pixels_np[:, ::-1].T.astype('uint8')*255)
+im.save("q5_raw.png")
+# im.show()
