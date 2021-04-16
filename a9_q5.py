fixing jacobian calculation

a9_q5.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy
 
 def tinkerbell(x,y, c_1 = -0.3, c_2=-0.6, c_3=2, c_4=0.5):
     return (x*x - y*y + c_1 * x + c_2 * y,
@@ -52,22 +53,16 @@ c_3 = 6
 for i in range(1, N+1):
     x,y =  ikeda(values[i - 1, 0], values[i - 1, 1], r=R, c_1=c_1, c_2=c_2, c_3=c_3)
     values[i, :] = (x,y)
-    tau = 1/(1 + x**2 + y**2)
-    tau_sq_neg = -1 * tau**2
-    dtaudx = tau_sq_neg * 2 * x
-    dtaudy = tau_sq_neg * 2 * y
-
-    xdx = c_2 * (np.cos(tau) - x*np.sin(tau)*dtaudx - y*np.cos(tau)*dtaudx )
-    xdy = -1 * c_2 * (x*np.sin(tau)*dtaudy + np.sin(tau) + y* np.cos(tau)*dtaudy)
-    ydx = c_2 * (np.sin(tau) + x * np.cos(tau)*dtaudx - y*np.sin(tau)*dtaudx)
-    ydy = c_2 * (x * np.cos(tau)*dtaudy + np.cos(tau) - y*np.sin(tau)*dtaudy)
-    J = np.array([[xdx, ydx], [xdy, ydy]])
+    J=np.array([[0.9000*np.cos(6/(x**2 + y**2 + 1) - 0.4000) + (10.8000*x**2*np.sin(6/(x**2 + y**2 + 1) - 0.4000))/(x**2 + y**2 + 1)**2 - (10.8000*x*y*np.cos(6/(x**2 + y**2 + 1) - 0.4000))/(x**2 + y**2 + 1)**2, 0.9000*np.sin(6/(x**2 + y**2 + 1) - 0.4000) - (10.8000*y**2*np.cos(6/(x**2 + y**2 + 1) - 0.4000))/(x**2 + y**2 + 1)**2 + (10.8000*x*y*np.sin(6/(x**2 + y**2 + 1) - 0.4000))/(x**2 + y**2 + 1)**2],
+        [(10.8000*x**2*np.cos(6/(x**2 + y**2 + 1) - 0.4000))/(x**2 + y**2 + 1)**2 - 0.9000*np.sin(6/(x**2 + y**2 + 1) - 0.4000) + (10.8000*x*y*np.sin(6/(x**2 + y**2 + 1) - 0.4000))/(x**2 + y**2 + 1)**2, 0.9000*np.cos(6/(x**2 + y**2 + 1) - 0.4000) + (10.8000*y**2*np.sin(6/(x**2 + y**2 + 1) - 0.4000))/(x**2 + y**2 + 1)**2 + (10.8000*x*y*np.cos(6/(x**2 + y**2 + 1) - 0.4000))/(x**2 + y**2 + 1)**2]])
 
     W, r = np.linalg.qr(np.dot(J, W))
     lyapunov_ests[i-1, :] =   r[0,0], r[1,1]
 print("ikeda map lyapunov exponents")
 lyapunov_exps = np.mean(np.log(np.abs(lyapunov_ests)), axis=0)
 print(lyapunov_exps)
+plt.scatter(values[:, 0], values[:, 1], lw=0.01)
+plt.show()
 
 
 values[:, :] = 0
@@ -114,3 +109,4 @@ lyapunov_exps = np.mean(np.log(np.abs(lyapunov_ests)), axis=0)
 print(lyapunov_exps)
 # plt.plot(values[:, 0], values[:, 1])
 # plt.show()
+